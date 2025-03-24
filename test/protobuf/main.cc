7cf704efda5dee73eedabd42d7572b90b8d7fc54
Theodoreshub <shijialonga@gmail.com>
2025-03-24 09:45:00 -0700

    protobuf2

--- test/protobuf/main.cc
+++ test/protobuf/main.cc
@@ -5,6 +5,26 @@
 using namespace fixbug;
 
 int main(){
+    // LoginResponse rsp;
+    // ResultCode *rc= rsp.mutable_result();
+    // rc->set_errcode(1);
+    // rc->set_errmsg("登录失败");
+
+    GetFriendListResponse rsp;
+    // mutable 是拿里面的 message 字段的
+    ResultCode *rc = rsp.mutable_result();
+    rc->set_errcode(0);
+
+    // add 是往 reapted 的字段新添加一个成员
+    User *user1 = rsp.add_friend_list();
+    user1->set_name("zhangsan");
+    user1->set_age(20);
+    user1->set_sex(User::MAN);
+    
+    std::cout << rsp.friend_list_size() << std::endl;
+}
+
+int main1(){
     LoginRequest req;
     req.set_name("zhangsan");
     req.set_pwd("123456");
